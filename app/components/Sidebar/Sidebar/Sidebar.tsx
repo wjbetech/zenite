1b1b965f87e82786039830066b8f7cc6d89cbc75
wjbetech <wjbetech@gmail.com>
2024-04-09 12:59:30 +0900

    small refactor, leaving dark for later

--- app/components/Sidebar/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar/Sidebar.tsx
@@ -13,51 +13,47 @@ import getMenuIcon from "@/app/utils/icons";
 import { useGlobalState } from "@/app/context/globalProvider";
 
 export default function Sidebar() {
-
   const { theme } = useGlobalState();
 
   console.log(theme);
 
-	const router = useRouter();
-	const pathName = usePathname();
-	const handleClick = (link: string) => {
-		router.push(link);
-	};
+  const router = useRouter();
+  const pathName = usePathname();
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
 
-	return (
+  return (
     // entire sidebar
-		<div className={`relative w-[15rem] flex flex-col justify-between border-none bg-slate-200 p-2 ${theme === "light" ? "bg-slate-200" : "bg-slate-700 text-gray-100"}`}>
+    <div className="relative w-[15rem] flex flex-col justify-between border-none bg-slate-200 p-2" w>
       {/* user section */}
-			<div className={`flex px-4 items-center gap-4 justify-evenly rounded-lg cursor-pointer hover:bg-slate-300 p-2 ${theme === "dark" ? "hover:bg-slate-600" : ""}`}>
-				<div className="image w-[33%]">
-					<Image
-						src="/avatar.jpg"
-						alt="stock avatar"
-						width={50}
-						height={50}
-						className="rounded-full flex-shrink-0 inline-block overflow-hidden transition-all ease-in-out"
-					/>
-				</div>
-				<div className="w-[66%] text-center">
-					<h1 className="">
-						<p className="text-sm">John Smith</p>
-            <p className={`text-xs text-gray-500 ${theme === "dark" ? "text-gray-300/75" : ""}`}>Software Eng.</p>
-					</h1>
-				</div>
-			</div>
+      <div className="flex px-4 items-center gap-4 justify-evenly rounded-lg cursor-pointer hover:bg-slate-300 p-2">
+        <div className="image w-[33%]">
+          <Image
+            src="/avatar.jpg"
+            alt="stock avatar"
+            width={50}
+            height={50}
+            className="rounded-full flex-shrink-0 inline-block overflow-hidden transition-all ease-in-out"
+          />
+        </div>
+        <div className="w-[66%] text-center">
+          <h1 className="">
+            <p className="text-sm">John Smith</p>
+            <p className="text-xs text-gray-500">Software Eng.</p>
+          </h1>
+        </div>
+      </div>
       {/* menu items */}
       <div className="workspace">
-        <h5 className={`text-slate-500 mb-2 pb-2 border-b-2 border-slate-400/50 ${theme === "dark" ? "text-gray-300" : ""}`}>WORKSPACE</h5>
+        <h5 className="text-slate-500 mb-2 pb-2 border-b-2 border-slate-400/50">WORKSPACE</h5>
         <ul className="cursor-pointer relative mb-10">
           {menu.map((item) => {
             const link = item.link;
             return (
               <li
                 key={item.id}
-                className={`flex items-baseline gap-2 p-2 hover:bg-white/10 text-slate-500 w-full rounded-md ${
-                  pathName === link ? "bg-slate-300 rounded-md" : ""
-                 } ${theme === "dark" ? "bg-none hover:bg-slate-600/50" : "" }
-                }`}
+                className="flex items-baseline gap-2 p-2 hover:bg-white/10 text-slate-500 w-full rounded-md"
                 onKeyDown={() => link}
               >
                 {getMenuIcon(item.icon)}
@@ -70,7 +66,7 @@ export default function Sidebar() {
         </ul>
       </div>
       {/* sign out button */}
-			<button type="button">Sign Out</button>
-		</div>
-	);
+      <button type="button">Sign Out</button>
+    </div>
+  );
 }
