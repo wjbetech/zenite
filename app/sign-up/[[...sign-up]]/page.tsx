5625802e7b3fe72295c2bf0c213d60576b12334c
wjbetech <wjbetech@gmail.com>
2024-04-15 21:50:27 +0900

    Fixed some clerk routing

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <SignUp />;
+    </div>
+  );
 }
