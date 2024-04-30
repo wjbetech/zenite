36605d0b9d56d556c8c03043ca858e3a01a8625a
wjbetech <wjbetech@gmail.com>
2024-04-30 13:45:03 +0900

    Patched clerk to core-2

--- app/components/Modal/TaskModal.tsx
+++ app/components/Modal/TaskModal.tsx
@@ -1,69 +1,69 @@
-"use client";
-
-import React, { useState } from "react";
-import { currentUser } from "@clerk/nextjs";
+import type React from "react";
+import { useState } from "react";
 
 // types for our task modal inputs
 interface TaskState {
-	title: string;
-	description: string;
-	date: string;
-	isCompleted: boolean;
-	userId?: string;
+  title: string;
+  description: string;
+  date: string;
+  isCompleted: boolean;
+  userId?: string;
 }
 
-export default async function TaskModal() {
-	// clerk user info
-	const user = await currentUser();
+export default function TaskModal() {
+  // get clerk user info
 
-	// states for tasks
-	const [taskState, setTaskState] = useState<TaskState>({
-		title: "",
-		description: "",
-		date: new Date().toISOString().slice(0, 16),
-		isCompleted: false,
-		// not sure I understand why we check for userId here when our app/route already checks
-		userId: user?.id,
-	});
+  // states for tasks
+  const [taskState, setTaskState] = useState<TaskState>({
+    title: "",
+    description: "",
+    date: new Date().toISOString().slice(0, 16),
+    isCompleted: false,
+    // not sure I understand why we check for userId here when our app/route already checks
+  });
 
-	// dynamically handle data changes
-	const handleInputChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value, type, checked } = event.target;
-		const newValue = type === "checkbox" ? checked : value;
-		setTaskState({ ...taskState, [name]: newValue });
-	};
+  // dynamically handle data changes
+  const handleInputChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setTaskState({ ...taskState, [name]: value });
+  };
 
-	// handle submissions
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log(taskState);
-	};
+  // handle submissions
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(taskState);
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<h1>Create Task</h1>
-			<div className="title">
-				<label htmlFor="title">Title</label>
-				<input
-					type="text"
-					name="title"
-					id="title"
-					value={taskState.title}
-					onChange={(e) => handleInputChanges(e)}
-					placeholder="Set up PrismaDB..."
-				/>
-			</div>
-			<div className="description">
-				<label htmlFor="description">Description</label>
-				<input
-					type="text"
-					name="description"
-					id="description"
-					value={taskState.description}
-					onChange={(e) => handleInputChanges(e)}
-					placeholder="Set up basic Prisma processes..."
-				/>
-			</div>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Create Task</h1>
+      <div className="title">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={taskState.title}
+          onChange={(e) => handleInputChanges(e)}
+          placeholder="Set up PrismaDB..."
+        />
+      </div>
+      <div className="description">
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={taskState.description}
+          onChange={(e) => handleInputChanges(e)}
+          placeholder="Set up basic Prisma processes..."
+        />
+      </div>
+      <div className="submit-button">
+        <button type="submit" className="submit-button">
+          Add Task
+        </button>
+      </div>
+    </form>
+  );
 }
--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-// publicRoutes sets bypass for auth
-// TODO: only serve main content when user is authenticated
-export default authMiddleware({});
-
-export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
