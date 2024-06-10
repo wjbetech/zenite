f5a34bb726b3f96382213970d3bb27db1519cb0d
wjbetech <wjbetech@gmail.com>
2024-06-11 01:38:37 +0900

    Fixed up modal styling and buggy modal close actions

--- app/components/Modals/AddTaskModal.tsx
+++ app/components/Modals/AddTaskModal.tsx
@@ -11,8 +11,8 @@ export default function AddTaskModal({ content }: Props) {
   const { toggleModal } = useGlobalState();
 
   return (
-    <div onClick={toggleModal}>
-      <div className="modal-content">{content}</div>
+    <div className="fixed flex top-0 left-0 w-screen h-screen z-10 bg-black/50">
+      <div className="bg-white m-auto justify-center align-middle w-[400px] rounded-md">{content}</div>
     </div>
   );
 }
