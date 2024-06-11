47c18d38d74cb302b6508f4d238b6e09300fc80d
wjbetech <wjbetech@gmail.com>
2024-06-11 12:45:00 +0900

    Fixed up modal close on escape functionality

--- app/components/Modals/AddTaskModal.tsx
+++ app/components/Modals/AddTaskModal.tsx
@@ -2,6 +2,7 @@
 
 import type React from "react";
 import { useGlobalState } from "@/app/context/globalProvider";
+import { useEffect } from "react";
 
 interface Props {
   content: React.ReactNode;
@@ -9,6 +10,24 @@ interface Props {
 
 export default function AddTaskModal({ content }: Props) {
   const { toggleModal } = useGlobalState();
+
+  const handleEscape = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      toggleModal();
+    }
+  };
+
+  // cannot pass handleEscape directly to the wrapper div because
+  // global keyboard events should be attached to the document
+  // or window object
+  useEffect(() => {
+    document.addEventListener("keydown", handleEscape);
+
+    // clean up
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [handleEscape]);
 
   return (
     <div className="fixed flex top-0 left-0 w-screen h-screen z-10 bg-black/50">
