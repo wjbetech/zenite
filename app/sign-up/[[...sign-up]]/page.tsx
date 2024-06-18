e68415a72fe168d1a173bb2816c0948b9c5c9fbc
William East <wjbetech@gmail.com>
2024-06-18 15:20:49 +0900

    Fixing deployment bugs #1 FaX import

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="flex justify-center h-screen items-center">
-      <SignUp routing="hash" />;
-    </div>
-  );
+	return (
+		<div className="flex justify-center h-screen items-center">
+			<SignUp routing="hash" />
+		</div>
+	);
 }
--- app/utils/icons.js
+++ app/utils/icons.js
@@ -1,22 +1,23 @@
-import { FaList, FaPlus, FaPlay, FaStop, FaCog, FaX } from "react-icons/fa";
+import { FaList, FaPlus, FaPlay, FaStop, FaCog } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
 
 const getMenuIcon = (icon) => {
-  switch (icon) {
-    case "home":
-      return <FaList />;
-    case "add":
-      return <FaPlus />;
-    case "active":
-      return <FaPlay />;
-    case "completed":
-      return <FaStop />;
-    case "settings":
-      return <FaCog />;
-    case "delete":
-      return <FaX />;
-    default:
-      return null;
-  }
+	switch (icon) {
+		case "home":
+			return <FaList />;
+		case "add":
+			return <FaPlus />;
+		case "active":
+			return <FaPlay />;
+		case "completed":
+			return <FaStop />;
+		case "settings":
+			return <FaCog />;
+		case "delete":
+			return <FaX />;
+		default:
+			return null;
+	}
 };
 
 export default getMenuIcon;
