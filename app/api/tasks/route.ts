210e0986a232645fdb9e807a401fc4ad0214fcbb
William East <wjbetech@gmail.com>
2024-06-18 16:06:54 +0900

    Fixed bugs - DB bug still exists

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,112 +1,115 @@
 import prisma from "@/app/utils/connectDB";
-import { User, auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 // POST requests with data validation (fin)
 export async function POST(req: Request) {
-  try {
-    // make sure a user is logged in before POST
-    const { userId } = auth();
+	try {
+		// make sure a user is logged in before POST
+		const { userId } = auth();
 
-    // send unauth'd user away
-    if (!userId) {
-      return NextResponse.json({
-        error: "Unauthorised",
-        status: redirect("/login"),
-      });
-    }
+		// send unauth'd user away
+		if (!userId) {
+			return NextResponse.json({
+				error: "Unauthorised",
+				status: redirect("/login"),
+			});
+		}
 
-    // gather and validate data from inputs
-    const { title, description, date, completed } = await req.json();
+		// gather and validate data from inputs
+		const { title, description, date, completed } = await req.json();
 
-    if (!title || !description) {
-      return NextResponse.json({
-        error: "Missing required fields",
-        status: 400,
-      });
-    }
+		if (!title || !description) {
+			return NextResponse.json({
+				error: "Missing required fields",
+				status: 400,
+			});
+		}
 
-    if (title.length < 3) {
-      return NextResponse.json({
-        error: "Title must be at least 3 characters",
-        status: 400,
-      });
-    }
+		if (title.length < 3) {
+			return NextResponse.json({
+				error: "Title must be at least 3 characters",
+				status: 400,
+			});
+		}
 
-    if (description.length < 10) {
-      return NextResponse.json({
-        error: "Description must be at least 15 characters",
-        status: 400,
-      });
-    }
+		if (description.length < 10) {
+			return NextResponse.json({
+				error: "Description must be at least 15 characters",
+				status: 400,
+			});
+		}
 
-    // build our prisma task
-    const task = await prisma.task.create({
-      data: {
-        title,
-        description,
-        date,
-        isCompleted: completed,
-        userId,
-      },
-    });
+		// build our prisma task
+		const task = await prisma.task.create({
+			data: {
+				title,
+				description,
+				date,
+				isCompleted: completed,
+				userId,
+			},
+		});
 
-    // return the prisma task in JSON
-    return NextResponse.json(task);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
+		// return the prisma task in JSON
+		return NextResponse.json(task);
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{ error: "Internal Server Error" },
+			{ status: 500 },
+		);
+	}
 }
 
 // GET requests with userId protection (fin)
 export async function GET() {
-  // find userId or error if no userId
-  const { userId } = auth();
-  if (!userId) {
-    return NextResponse.json({
-      error: "Unauthorised",
-      status: 401,
-      // status: redirect("/login"),
-    });
-  }
-  // match userId to logged in userId posts
-  const tasks = await prisma.task.findMany({
-    where: { userId },
-  });
-  // return the matching tasks in JSON
-  return NextResponse.json(tasks);
+	// find userId or error if no userId
+	const { userId } = auth();
+	if (!userId) {
+		return NextResponse.json({
+			error: "Unauthorised",
+			status: 401,
+			// status: redirect("/login"),
+		});
+	}
+	// match userId to logged in userId posts
+	const tasks = await prisma.task.findMany({
+		where: { userId },
+	});
+	// return the matching tasks in JSON
+	return NextResponse.json(tasks);
 }
 
 // PUT requests for updating task status
 export async function PUT(req: Request) {
-  try {
-    const { userId } = auth();
-    const { isCompleted, id } = await req.json();
+	try {
+		const { userId } = auth();
+		const { isCompleted, id } = await req.json();
 
-    if (!userId) {
-      return NextResponse.json({
-        error: "Unauthorised",
-        status: 401,
-      });
-    }
+		if (!userId) {
+			return NextResponse.json({
+				error: "Unauthorised",
+				status: 401,
+			});
+		}
 
-    const task = await prisma.task.update({
-      where: {
-        id,
-      },
-      data: {
-        isCompleted,
-      },
-    });
+		const task = await prisma.task.update({
+			where: {
+				id,
+			},
+			data: {
+				isCompleted,
+			},
+		});
 
-    return NextResponse.json(task);
-  } catch (error) {
-    console.log("Could not update task!", error);
-    return NextResponse.json({
-      error: "Could not update task",
-      status: 500,
-    });
-  }
+		return NextResponse.json(task);
+	} catch (error) {
+		console.log("Could not update task!", error);
+		return NextResponse.json({
+			error: "Could not update task",
+			status: 500,
+		});
+	}
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 
 // clerk
 import { ClerkProvider } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs/server";
 
 // nextTopLoader
 import NextTopLoader from "nextjs-toploader";
@@ -19,31 +18,34 @@ import ContextProvider from "./providers/ContextProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zenite | Your New Task Manager.",
-  description: "A NextJs task management application hosted on Vercel.",
+	title: "Zenite | Your New Task Manager.",
+	description: "A NextJs task management application hosted on Vercel.",
 };
 
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  const { userId } = auth();
-  const user = await currentUser();
-
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <NextTopLoader height={4} color="#B57EDC" easing="cubic-bezier(.53, .21, 0, 1)" />
-          <div className="flex h-full">
-            <ContextProvider>
-              <Sidebar />
-              <div className="w-full px-2 ml-4 sm:ml-[260px] p-6 mr-4">{children}</div>
-            </ContextProvider>
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					<NextTopLoader
+						height={4}
+						color="#B57EDC"
+						easing="cubic-bezier(.53, .21, 0, 1)"
+					/>
+					<div className="flex h-full">
+						<ContextProvider>
+							<Sidebar />
+							<div className="w-full px-2 ml-4 sm:ml-[260px] p-6 mr-4">
+								{children}
+							</div>
+						</ContextProvider>
+					</div>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }
